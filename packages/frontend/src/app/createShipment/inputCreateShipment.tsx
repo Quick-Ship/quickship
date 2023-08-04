8d33b96ab9939cc8b7f874d31f3072423f9f8118
andres <mucino.andres1990@gmail.com>
2023-08-04 01:00:53 -0600

    add mutation for create shipment

--- packages/frontend/src/app/createShipment/inputCreateShipment.tsx
+++ packages/frontend/src/app/createShipment/inputCreateShipment.tsx
@@ -6,7 +6,10 @@ import {
   EuiFormRow,
   EuiHorizontalRule,
   EuiPanel,
+  EuiSuperSelect,
+  EuiText,
 } from "@elastic/eui";
+import { useState } from "react";
 
 interface InputCreateShipmentProps {
   register: any;
@@ -23,34 +26,117 @@ export const InputCreateShipment: React.FC<InputCreateShipmentProps> = ({
 
     setValue(name, value);
   };
+
+  const options = [
+    {
+      value: "envelope",
+      inputDisplay: "Sobre",
+      dropdownDisplay: (
+        <>
+          <strong>Sobre</strong>
+          <EuiText size="s" color="subdued">
+            <p>Envia sobres, llaves, libros</p>
+          </EuiText>
+        </>
+      ),
+    },
+    {
+      value: "box_small",
+      inputDisplay: "Caja chica",
+      dropdownDisplay: (
+        <>
+          <strong>Caja chica</strong>
+          <EuiText size="s" color="subdued">
+            <p>
+              Caracteristicas del paquete: Largo: 40, Ancho: 30, Alto: 25 y
+              hasta 1kg
+            </p>
+          </EuiText>
+        </>
+      ),
+    },
+    {
+      value: "box_medium",
+      inputDisplay: "Caja mediana",
+      dropdownDisplay: (
+        <>
+          <strong>Caja mediana</strong>
+          <EuiText size="s" color="subdued">
+            <p>
+              Caracteristicas del paquete: Largo: 30, Ancho: 40, Alto: 30 y
+              hasta 15kg
+            </p>
+          </EuiText>
+        </>
+      ),
+    },
+    {
+      value: "box_medium",
+      inputDisplay: "Caja grande",
+      dropdownDisplay: (
+        <>
+          <strong>Caja grande</strong>
+          <EuiText size="s" color="subdued">
+            <p>
+              Caracteristicas del paquete: Largo: 40, Ancho: 50, Alto: 40 y
+              hasta 25kg
+            </p>
+          </EuiText>
+        </>
+      ),
+    },
+  ];
+
+  const [valueselect, setValueselect] = useState("");
+
+  const onChangeSelect = (value: any) => {
+    setValueselect(value);
+  };
+
   return (
     <EuiFlexGroup>
       <EuiFlexItem>
         <EuiPanel>
           <strong>Datos Generales</strong>
           <EuiHorizontalRule />
-          <EuiFormRow>
+          <EuiFormRow id="1">
             <EuiFieldText
               name="instructions"
               onChange={onChange}
-              inputRef={register("street")}
-              placeholder="Calle"
+              inputRef={register("instructions")}
+              placeholder="Instrucciones de recolección"
             />
           </EuiFormRow>
-          <EuiFormRow>
-            <EuiFieldText
-              name="packages type"
-              onChange={onChange}
-              inputRef={register("street")}
-              placeholder="Calle"
-            />
-          </EuiFormRow>
-          <EuiFormRow>
+          <EuiFormRow id="3">
             <EuiFieldText
               name="idClient"
               onChange={onChange}
-              inputRef={register("street")}
-              placeholder="Calle"
+              inputRef={register("idClient")}
+              placeholder="Id del cliente"
+            />
+          </EuiFormRow>
+          <EuiFormRow id="9">
+            <EuiFieldText
+              name="warehouseShipmentId"
+              onChange={onChange}
+              inputRef={register("warehouseShipmentId")}
+              placeholder="Almacen del cliente "
+            />
+          </EuiFormRow>
+          <EuiFormRow>
+            <EuiSuperSelect
+              id="0"
+              options={options}
+              valueOfSelected={valueselect}
+              placeholder="Selecciona el tipo de paquete"
+              name="packageType"
+              onChange={(value) => {
+                onChangeSelect(value);
+                setValue("packageType", value);
+                register("packageType");
+              }}
+              itemLayoutAlign="top"
+              hasDividers
             />
           </EuiFormRow>
         </EuiPanel>
@@ -60,7 +146,7 @@ export const InputCreateShipment: React.FC<InputCreateShipmentProps> = ({
         <EuiPanel>
           <strong>Direccion</strong>
           <EuiHorizontalRule />
-          <EuiFormRow>
+          <EuiFormRow id="4">
             <EuiFieldText
               name="street"
               onChange={onChange}
@@ -68,7 +154,23 @@ export const InputCreateShipment: React.FC<InputCreateShipmentProps> = ({
               placeholder="Calle"
             />
           </EuiFormRow>
-          <EuiFormRow>
+          <EuiFormRow id="10">
+            <EuiFieldText
+              name="externalNumber"
+              onChange={onChange}
+              inputRef={register("externalNumber")}
+              placeholder="Numero exterior"
+            />
+          </EuiFormRow>
+          <EuiFormRow id="11">
+            <EuiFieldText
+              name="internalNumber"
+              onChange={onChange}
+              inputRef={register("internalNumber")}
+              placeholder="Numero interior"
+            />
+          </EuiFormRow>
+          <EuiFormRow id="5">
             <EuiFieldText
               name="neigthboorhood"
               onChange={onChange}
@@ -76,7 +178,7 @@ export const InputCreateShipment: React.FC<InputCreateShipmentProps> = ({
               placeholder="Colonia"
             />
           </EuiFormRow>
-          <EuiFormRow>
+          <EuiFormRow id="6">
             <EuiFieldText
               name="municipality"
               onChange={onChange}
@@ -84,7 +186,7 @@ export const InputCreateShipment: React.FC<InputCreateShipmentProps> = ({
               placeholder="Delegación o municipio"
             />
           </EuiFormRow>
-          <EuiFormRow>
+          <EuiFormRow id="7">
             <EuiFieldText
               name="state"
               onChange={onChange}
@@ -92,12 +194,28 @@ export const InputCreateShipment: React.FC<InputCreateShipmentProps> = ({
               placeholder="Ciudad"
             />
           </EuiFormRow>
-          <EuiFormRow>
+          <EuiFormRow id="8">
             <EuiFieldText
               name="zipCode"
               onChange={onChange}
               inputRef={register("zipCode")}
               placeholder="Codigo postal"
+            />
+          </EuiFormRow>
+          <EuiFormRow id="12">
+            <EuiFieldText
+              name="latitude"
+              onChange={onChange}
+              inputRef={register("latitude")}
+              placeholder="Latitud"
+            />
+          </EuiFormRow>
+          <EuiFormRow id="13">
+            <EuiFieldText
+              name="longitude"
+              onChange={onChange}
+              inputRef={register("longitude")}
+              placeholder="Longitud"
             />
           </EuiFormRow>
         </EuiPanel>
--- packages/frontend/src/app/createShipment/page.tsx
+++ packages/frontend/src/app/createShipment/page.tsx
@@ -1,21 +1,25 @@
 "use client";
 
-import { GeneralForm, Header } from "@/components";
+import { Button, Header } from "@/components";
 import {
-  EuiButton,
-  EuiCode,
-  EuiFieldText,
-  EuiFlexGroup,
-  EuiFlexItem,
   EuiForm,
-  EuiFormRow,
   EuiHorizontalRule,
+  EuiPageHeader,
   EuiPageHeaderContent,
   EuiPanel,
-  EuiText,
+  EuiSkeletonText,
 } from "@elastic/eui";
 import { InputCreateShipment } from "./inputCreateShipment";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import {
+  AddPackagesToShipments,
+  CreatePackages,
+  GenerateShipment,
+  graphQLClient,
+} from "@/graphql";
+import { nanoid } from "nanoid";
 
 export default function CreateShipment() {
   const {
@@ -25,21 +29,159 @@ export default function CreateShipment() {
     formState: { errors },
   } = useForm();
 
+  const { mutate: mutateGeneratePackage, status: statusGeneratePakcage } =
+    useMutation({
+      mutationKey: ["createOnePackages"],
+      mutationFn: (packages: any) => {
+        return graphQLClient.request(CreatePackages, packages);
+      },
+    });
+
+  const { mutate: mutateGenerateShipment, status: statusGenerateShipment } =
+    useMutation({
+      mutationKey: ["createShipment"],
+      mutationFn: (shipment: any) => {
+        return graphQLClient.request(GenerateShipment, shipment);
+      },
+    });
+
+  const { mutate: mutateAddPackageShipment, status: statusAddPackageShipment } =
+    useMutation({
+      mutationKey: ["addPackagesShipment"],
+      mutationFn: (addPackages: any) => {
+        return graphQLClient.request(AddPackagesToShipments, addPackages);
+      },
+    });
+
   const onSubmit = (data: any) => {
     console.log(data);
+    let packagesType: any;
+    if (data.packageType === "envelope") {
+      packagesType = { heigth: 1, length: 1, width: 1, weigth: 1 };
+    }
+    if (data.packageType === "box_small") {
+      packagesType = { heigth: 25, length: 40, width: 30, weigth: 1 };
+    }
+    if (data.packageType === "box_medium") {
+      packagesType = { heigth: 30, length: 30, width: 40, weigth: 15 };
+    }
+    if (data.packageType === "box_big") {
+      packagesType = { heigth: 40, length: 40, width: 50, weigth: 25 };
+    }
+
+    mutateGeneratePackage(
+      {
+        input: {
+          weigth: packagesType.weigth,
+          width: packagesType.width,
+          heigth: packagesType.heigth,
+          length: packagesType.length,
+          guide: nanoid(),
+          contact: {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            phone: "+52" + data.phone,
+            email: data.email,
+          },
+          direction: {
+            street: data.street,
+            neigthboorhood: data.neigthboorhood,
+            municipality: data.municipality,
+            state: data.state,
+            zipCode: data.zipCode,
+            externalNumber: data.externalNumber,
+            internalNumber: data.internalNumber,
+            latitude: Number(data.latitude),
+            longitude: Number(data.longitude),
+          },
+          idClient: Number(data.idClient),
+        },
+      },
+      {
+        onSuccess: (datasuccess: any) => {
+          console.log(datasuccess, "genera guides");
+          mutateGenerateShipment(
+            {
+              input: {
+                comments: data.instructions,
+                clientId: datasuccess.createDelivery.clientId,
+                warehouseShipmentId: Number(data.warehouseShipmentId),
+              },
+            },
+            {
+              onSuccess: (data: any) => {
+                console.log(data, "genera un envio");
+                mutateAddPackageShipment(
+                  {
+                    input: {
+                      shipmentId: data.generateShipment.id,
+                      guides: datasuccess.createDelivery.guide,
+                    },
+                  },
+                  {
+                    onError: (error) => {
+                      console.log(error);
+                    },
+                    onSuccess: (data) => {
+                      console.log(data, "agrega packages");
+                    },
+                  }
+                );
+              },
+              onError: (error) => {
+                console.log(error);
+              },
+            }
+          );
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      }
+    );
   };
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted)
+    return (
+      <>
+        <EuiPanel style={{ margin: "2vh" }}>
+          <EuiPageHeader>
+            <EuiSkeletonText
+              lines={1}
+              size={"relative"}
+              // isLoading={isLoading}
+            ></EuiSkeletonText>
+          </EuiPageHeader>
+          <EuiSkeletonText
+            lines={6}
+            size={"m"}
+            // isLoading={isLoading}
+          ></EuiSkeletonText>
+        </EuiPanel>
+      </>
+    );
+
   return (
     <EuiPageHeaderContent>
-      <EuiPanel style={{ margin: "2vh" }}>
-        <Header title={`Ordenes ()`}>{""}</Header>
-        <EuiHorizontalRule />
+      <EuiPanel style={{ margin: "2vh", height: "700px" }}>
         <EuiForm component="form" onSubmit={handleSubmit(onSubmit)}>
+          <Header title={`Crear envio`}>
+            <Button type="submit" size="m" fill>
+              Crear
+            </Button>
+          </Header>
+          <EuiHorizontalRule />
+
           <InputCreateShipment
             register={register}
             setValue={setValue}
             errors={errors}
           />
-          <EuiButton type="submit">submit</EuiButton>
         </EuiForm>
       </EuiPanel>
     </EuiPageHeaderContent>
